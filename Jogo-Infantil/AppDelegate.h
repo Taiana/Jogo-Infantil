15f3844101d0fce2bc8bedcb105ee32119faf893
taiana <taianamoreira@gmail.com>
2015-04-13 22:32:02 -0300

    Setup inicial

---
+++ Jogo-Infantil/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Jogo-Infantil
+//
+//  Created by Dantas on 13/04/15.
+//  Copyright (c) 2015 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Jogo-InfantilTests/Jogo_InfantilTests.m
@@ -0,0 +1,40 @@
+//
+//  Jogo_InfantilTests.m
+//  Jogo-InfantilTests
+//
+//  Created by Dantas on 13/04/15.
+//  Copyright (c) 2015 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Jogo_InfantilTests : XCTestCase
+
+@end
+
+@implementation Jogo_InfantilTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
