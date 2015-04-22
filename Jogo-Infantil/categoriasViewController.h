0e0e4b128b42f1e1ffef1e536699293fb2e27663
taiana <taianamoreira@gmail.com>
2015-04-22 00:54:57 -0300

    Corrigindo fontes que não estavam lendo.

---
+++ Jogo-Infantil/categoriasViewController.h
@@ -0,0 +1,22 @@
+//
+//  categoriasViewController.h
+//  Jogo-Infantil
+//
+//  Created by Dantas on 22/04/15.
+//  Copyright (c) 2015 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface categoriasViewController : UIViewController
+
+//@property (weak, nonatomic) IBOutlet UILabel *animaisLabel;
+//@property (weak, nonatomic) IBOutlet UILabel *floresLabel;
+//@property (weak, nonatomic) IBOutlet UILabel *carrosLabel;
+//@property (weak, nonatomic) IBOutlet UILabel *letrasLabel;
+//@property (weak, nonatomic) IBOutlet UILabel *numerosLabel;
+//@property (weak, nonatomic) IBOutlet UILabel *formasLabel;
+//@property (weak, nonatomic) IBOutlet UILabel *categoriaLabel;
+
+
+@end
