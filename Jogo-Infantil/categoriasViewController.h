2c280d426624a56e0a73995e468e5a48678f4fae
taiana <taianamoreira@gmail.com>
2015-04-23 21:14:21 -0300

    Ajustes dos cabeçalhos dos arquivos

--- Jogo-Infantil/categoriasViewController.h
+++ Jogo-Infantil/categoriasViewController.h
@@ -2,7 +2,7 @@
 //  categoriasViewController.h
 //  Jogo-Infantil
 //
-//  Created by Dantas on 22/04/15.
+//  Created by Taiana on 22/04/15.
 //  Copyright (c) 2015 Home. All rights reserved.
 //
 
--- Jogo-InfantilTests/Jogo_InfantilTests.m
+++ Jogo-InfantilTests/Jogo_InfantilTests.m
@@ -2,7 +2,7 @@
 //  Jogo_InfantilTests.m
 //  Jogo-InfantilTests
 //
-//  Created by Dantas on 13/04/15.
+//  Created by Taiana on 13/04/15.
 //  Copyright (c) 2015 Home. All rights reserved.
 //
 
