dc3979064f138880e785a2f297a12ece6ea21e65
Taiana <taiana.moreira.quality@infoglobo.com.br>
2015-05-14 23:59:43 -0300

    Criando a classe view controller de animais

---
+++ Jogo-Infantil/animaisViewController.h
@@ -0,0 +1,13 @@
+//
+//  animaisViewController.h
+//  Jogo-Infantil
+//
+//  Created by Jose Dantas on 5/14/15.
+//  Copyright (c) 2015 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface animaisViewController : UIViewController
+
+@end
