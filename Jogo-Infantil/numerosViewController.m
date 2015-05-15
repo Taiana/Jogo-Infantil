5ae0599f380655841f1401a79eea6d0f70075bda
Taiana <taiana.moreira.quality@infoglobo.com.br>
2015-05-14 23:59:27 -0300

    Cairned a classe view controller de números

---
+++ Jogo-Infantil/numerosViewController.m
@@ -0,0 +1,40 @@
+//
+//  numerosViewController.m
+//  Jogo-Infantil
+//
+//  Created by Jose Dantas on 5/14/15.
+//  Copyright (c) 2015 Home. All rights reserved.
+//
+
+#import "numerosViewController.h"
+#import "globalVars.h"
+
+@interface numerosViewController ()
+
+@end
+
+@implementation numerosViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    GlobalVars *globals = [GlobalVars sharedInstance];
+    globals.vcOrigem = self;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
